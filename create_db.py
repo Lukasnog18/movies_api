566ee3b4aecd435662aff134881ffdc4c6a2738b
LukasNog18 <luksnogueira18@gmail.com>
2024-09-16 01:54:26 -0300

    Realizando pequenos ajustes necessários...

--- create_db.py
+++ create_db.py
@@ -1,8 +1,9 @@
 from app import create_app
-from app.database import create_mock_users, create_mock_movies
+from app.database import create_mock_users, create_mock_movies, db
 
 app = create_app()
 
 with app.app_context():
+    db.create_all()
     create_mock_users()
     create_mock_movies()
