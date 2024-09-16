1a7fd23f0788499925125d9eaa860814de2df97d
LukasNog18 <luksnogueira18@gmail.com>
2024-09-16 00:48:25 -0300

    Adicionando arquivo de testes e atualizando README para ## Testes

--- test_routes.py
+++ test_routes.py
@@ -1,6 +1,7 @@
 import pytest
 from flask_testing import TestCase
 from app import create_app, db
+from app.schema import Movie, User
 
 class TestBase(TestCase):
     def create_app(self):
@@ -12,7 +13,18 @@ class TestBase(TestCase):
     def setUp(self):
         """Configura o ambiente de teste antes de cada teste"""
         db.create_all()
-        # Adicione dados iniciais de teste aqui, se necessário
+
+        # Adiciona dados iniciais de teste
+        user1 = User(id=1, name='Lucas Nogueira', phone='8811111111', email='email@mock.com')
+        user2 = User(id=2, name='Lucas Ferreira', phone='8822222222', email='email@mock.com2')
+        db.session.add(user1)
+        db.session.add(user2)
+
+        movie1 = Movie(id=1, title='The Godfather', genre='Crime, Drama', year='1972', synopsis='The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.', director='Francis Ford Coppola')
+        movie2 = Movie(id=2, title='The Dark Knight', genre='Action, Crime, Drama', year='2008', synopsis='When the menace known as the Joker emerges, he creates chaos in Gotham. Batman must accept one of the greatest psychological tests of his ability to fight injustice.', director='Christopher Nolan')
+        db.session.add(movie1)
+        db.session.add(movie2)
+        db.session.commit()
 
     def tearDown(self):
         """Limpa o ambiente de teste após cada teste"""
@@ -33,9 +45,7 @@ class TestMovieAPI(TestBase):
         """Testa a rota para alugar um filme"""
         response = self.client.post('/rent/1/1')
         self.assert200(response)
-        self.assertEqual(len(response.json), 1)
-        self.assertIn('rental_date', response.json[0])
-        self.assertIn('rating', response.json[0])
+        self.assertEqual(response.json['message'], 'Movie 1 rented successfully by user 1')
 
     def test_rent_movie_already_rented(self):
         """Testa a tentativa de alugar um filme já alugado"""
@@ -49,7 +59,7 @@ class TestMovieAPI(TestBase):
         self.client.post('/rent/1/1')
         response = self.client.post('/movies/1/rate', json={'user_id': 1, 'rating': 5})
         self.assert200(response)
-        self.assertEqual(response.json['message'], 'Movie 1 rated 5 successfully')
+        self.assertEqual(response.json['message'], 'Movie 1 rated 5 successfully by user 1')
 
     def test_get_user_rents(self):
         """Testa a rota para obter os aluguéis de um usuário"""
